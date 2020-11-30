a9a0eb8cfb06db5c90d5c62f4d32f585a5c321db
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2020-11-30 16:08:56 +0100

    Add project files.

---
+++ NYCFlights2013/Models/Airlines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NYCFlight2013.Model
+{
+	public class Airlines
+	{
+		[Key]
+		public string carrier { get; set; }
+		public string name { get; set; }
+
+	}
+}
