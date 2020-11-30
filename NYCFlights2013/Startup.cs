2def31051aaa6211e9b1258a919438aebaf79d35
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2020-11-30 16:22:24 +0100

    Update Startup.cs
    qqq

--- NYCFlights2013/Startup.cs
+++ NYCFlights2013/Startup.cs
@@ -21,7 +21,7 @@ namespace NYCFlights2013
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		//
+		
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
