fe76274f8ef424074c1aff9a0d69a3f2b66092b9
ziad <ziadb.zoro@gmail.com>
2020-12-14 17:39:40 +0100

    rebuild

---
+++ NYCFlights2013Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NYCFlights2013Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
