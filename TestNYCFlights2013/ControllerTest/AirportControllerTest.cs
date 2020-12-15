77bbd327c12845eebc7270b1ee261678ce7ac365
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2020-12-15 11:10:47 +0100

    test have error

--- TestNYCFlights2013/ControllerTest/AirportControllerTest.cs
+++ TestNYCFlights2013/ControllerTest/AirportControllerTest.cs
@@ -15,5 +15,37 @@ namespace TestNYCFlights2013.ControllerTest
 		{
 		}
 		//TODO: Write test assertions for each method.
+		[Test]
+		public void TestGetAllAirports()
+		{
+			var airportsTest = controller.GetAllAirports();
+
+			int counter = 0;
+
+			foreach (var airTest in airportsTest)
+			{
+				string faaTest = airTest.faa;
+				string nameTest = airTest.name;
+				string latTest = airTest.lat;
+				string lonTest = airTest.lon;
+				string altTest = airTest.alt;
+				string tzTest = airTest.tz;
+				string dstTest = airTest.dst;
+				string tzoneTest = airTest.tzone;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 12)
+				{
+					if (faaTest == "17G" && nameTest == "Port Bucyrus-Crawford County Airport" && latTest == "41" && lonTest == "-83" 
+						&& altTest == "1003" && tzTest == "-5" && dstTest == "A" && tzoneTest == "America/New_York")
+					{
+						 Console.WriteLine(faaTest);
+						 Console.WriteLine(nameTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
 	}
 }
--- TestNYCFlights2013/ControllerTest/FlightControllerTest.cs
+++ TestNYCFlights2013/ControllerTest/FlightControllerTest.cs
@@ -15,5 +15,133 @@ namespace TestNYCFlights2013.ControllerTest
 		{
 		}
 		//TODO: Write test assertions for each method.
+		[Test]
+		public void TestGetNumOfFlightsM()
+		{
+			var numOfFlightsMTest = controller.GetNumOfFlightsM();
+
+			int counter = 0;
+
+			foreach (var numTest in numOfFlightsMTest)
+			{
+				string monthTest = numTest.month;
+				string numberTest = numTest.number;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 4)
+				{
+					if (monthTest == "5" && numberTest == "13692")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+
+		[Test]
+		public void TestGetMeantimeO()
+		{
+			var meantimeOTest = controller.GetMeantimeO();
+
+			int counter = 0;
+
+			foreach (var meanOTest in meantimeOTest)
+			{
+				string originTest = meanOTest.origin;
+				string meantimeTest = meanOTest.meantime;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 2)
+				{
+					if (originTest == "JFK" && meantimeTest == "235.3726")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetNumOfFlightsO()
+		{
+			var numOfFlightsMTest = controller.GetNumOfFlightsO();
+
+			int counter = 0;
+
+			foreach (var numMTest in numOfFlightsMTest)
+			{
+				string originTest = numMTest.origin;
+				string monthTest = numMTest.month;
+				string numberOTest = numMTest.numberO;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 11)
+				{
+					if (originTest == "EWR" && monthTest == "12" && numberOTest == "5084")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetTopdestM()
+		{
+			var topdestTest = controller.GetTopdest();
+
+			int counter = 0;
+
+			foreach (var topTest in topdestTest)
+			{
+				string destTest = topTest.dest;
+				string top10Test = topTest.top10;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 3)
+				{
+					if (destTest == "ATL" && top10Test == "10674")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetMeandelay()
+		{
+			var meandelayTest = controller.GetMeandelay();
+
+			int counter = 0;
+
+			foreach (var meanTest in meandelayTest)
+			{
+				string originTest = meanTest.origin;
+				string delayDTest = meanTest.delayD;
+				string delayATest = meanTest.delayA;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 2)
+				{
+					if (originTest == "JFK" && delayDTest == "8.7551" && delayATest == "-0.0746")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
 	}
 }
