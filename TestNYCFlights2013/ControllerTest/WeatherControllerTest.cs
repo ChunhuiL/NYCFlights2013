111549a36e697786a7ae4d0ee0ccb513d9331674
Angel Petrov <33376214+Angel266333@users.noreply.github.com>
2020-12-16 19:23:03 +0100

    Restore missing tests

--- TestNYCFlights2013/ControllerTest/WeatherControllerTest.cs
+++ TestNYCFlights2013/ControllerTest/WeatherControllerTest.cs
@@ -15,5 +15,145 @@ namespace TestNYCFlights2013.ControllerTest
 		{
 		}
 		//TODO: Write test assertions for each method.
+		[Test]
+		public void TestGetWeatherTemp()
+		{
+			var temp_attribute_celciusTest = controller.GetWeatherTemp();
+
+			int counter = 0;
+
+			foreach (var tacTest in temp_attribute_celciusTest)
+			{
+				string originTest = tacTest.origin;
+				string tempTest = tacTest.temp;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 5)
+				{
+					if (originTest == "EWR" && tempTest == "3.89")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetWeatherTempDate()
+		{
+			var temp_attribute_celcius_dateTest = controller.GetWeatherTempDate();
+
+			int counter = 0;
+
+			foreach (var tacdTest in temp_attribute_celcius_dateTest)
+			{
+				string dayTest = tacdTest.day;
+				string monthTest = tacdTest.month;
+				string yearTest = tacdTest.year;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 12)
+				{
+					if (dayTest == "1" && monthTest == "1" && yearTest == "2013")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetWeatherTempAtJFK()
+		{
+			var temp_attribute_celciusJFKTest = controller.GetWeatherTempAtJFK();
+
+			int counter = 0;
+
+			foreach (var tacjTest in temp_attribute_celciusJFKTest)
+			{
+				string originTest = tacjTest.origin;
+				string tempTest = tacjTest.temp;
+				string dayTest = tacjTest.day;
+				string monthTest = tacjTest.month;
+				string yearTest = tacjTest.year;
+
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 5)
+				{
+					if (originTest == "JFK" && tempTest == "3.89" && dayTest == "1"
+						&& monthTest == "1" && yearTest == "2013")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetDailyMeanTemp()
+		{
+			var temp_attribute_daily_meanTest = controller.GetDailyMeanTemp();
+
+			int counter = 0;
+
+			foreach (var tadmTest in temp_attribute_daily_meanTest)
+			{
+				string originTest = tadmTest.origin;
+				string dayTest = tadmTest.day;
+				string monthTest = tadmTest.month;
+				string yearTest = tadmTest.year;
+				string meanTest = tadmTest.mean;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 5)
+				{
+					if (originTest == "EWR" && meanTest == "3.33" && dayTest == "6"
+						&& monthTest == "1" && yearTest == "2013")
+					{
+						// Console.WriteLine(nameTest);
+						// Console.WriteLine(carrierTest);
+						Assert.Pass();
+					}
+				}
+				counter++;
+			}
+			Assert.Fail();
+		}
+		[Test]
+		public void TestGetDailyMeanTempJFK()
+		{
+			var temp_attribute_daily_mean_JFKTest = controller.GetDailyMeanTempJFK();
+
+			int counter = 0;
+
+			foreach (var tadmjTest in temp_attribute_daily_mean_JFKTest)
+			{
+				string originTest = tadmjTest.origin;
+				string dayTest = tadmjTest.day;
+				string monthTest = tadmjTest.month;
+				string yearTest = tadmjTest.year;
+				string meanTest = tadmjTest.mean;
+				// Check to see if value number 12 contains US Airways Inc with carrier value "US".
+				if (counter == 5)
+				{
+					if (originTest == "JFK" && meanTest == "3.06" && dayTest == "6"
+						&& monthTest == "1" && yearTest == "2013")
+					{
+
+						Assert.Pass();
+					}
+				}
+				Console.WriteLine(originTest);
+				Console.WriteLine(meanTest);
+				counter++;
+			}
+			Assert.Fail();
+		}
 	}
 }
